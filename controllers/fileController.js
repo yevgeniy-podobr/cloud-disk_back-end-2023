fb966cc035d32db07f39d412906f376184e6f8d9
zhenia.podobreev <zhenia.podobreev@cruxlab.com>
2023-09-28 17:01:03 +0300

    added api for search files

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -137,6 +137,17 @@ class FileController {
     }
   }
 
+  async searchFile(req, res) {
+    try {
+      const searchName = req.query.search
+      let files = await File.find({user: req.user.id})
+      files = files.filter(file => file.name.includes(searchName))
+      return res.json(files)
+    } catch (error) {
+      console.log(error)
+      return res.status(400).json({ message: 'Search error'})
+    }
+  }
 }
 
 module.exports = new FileController()
--- routes/file.routers.js
+++ routes/file.routers.js
@@ -8,5 +8,6 @@ router.get('', authMiddleware, fileController.getFiles)
 router.post('/upload', authMiddleware, fileController.uploadFile)
 router.get('/download', authMiddleware, fileController.downloadFile)
 router.delete('/', authMiddleware, fileController.deleteFile)
+router.get('/search', authMiddleware, fileController.searchFile)
 
 module.exports = router;
