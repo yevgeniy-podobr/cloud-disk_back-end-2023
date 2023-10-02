587d0141fb60e8e9bb67e88956e559c4747b15fb
zhenia.podobreev <zhenia.podobreev@cruxlab.com>
2023-10-02 13:31:24 +0300

    added functions upload and delete avater for server

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -3,7 +3,7 @@ const config = require("config")
 const fs = require('fs')
 const User = require('../models/User')
 const File = require('../models/File')
-const { type } = require('os')
+const uuid = require("uuid")
 
 class FileController {
   async createDir(req, res) {
@@ -146,6 +146,35 @@ class FileController {
     } catch (error) {
       console.log(error)
       return res.status(400).json({ message: 'Search error'})
+    }
+  }
+
+  async uploadAvatar(req, res) {
+    try {
+      const file = req.files.file
+      const user = await User.findById(req.user.id)
+      const avatarName = uuid.v4() + '.jpg'
+      file.mv(config.get("staticPath") + avatarName)
+      user.avatar = avatarName
+      await user.save()
+
+      return res.json(user)
+    } catch (error) {
+      console.log(error)
+      return res.status(400).json({ message: 'Upload avatar error'})
+    }
+  }
+
+  async deleteAvatar(req, res) {
+    try {
+      const user = await User.findById(req.user.id)
+      fs.unlinkSync(config.get("staticPath") + user.avatar)
+      user.avatar = null
+      await user.save()
+      return res.json(user)
+    } catch (error) {
+      console.log(error)
+      return res.status(400).json({ message: 'Delete avatar error'})
     }
   }
 }
